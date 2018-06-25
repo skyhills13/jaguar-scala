98e266652389c4393214a53d9a764ee1488df8ec
박소은[eg102] <skyhills13@gmail.com>
2018-06-25 23:01:41 +0900

    add case class for wish

--- src/main/scala/io/skyhills/jaguarscala/Domain.scala
+++ src/main/scala/io/skyhills/jaguarscala/Domain.scala
@@ -5,17 +5,9 @@ import com.github.nscala_time.time.Imports.DateTime
 /**
   * Created by soeunpark on 2018. 6. 3...
   */
-case class Corporation(
-    corpId: Int,
-    corpName: String,
-    isFavorite: Boolean
-)
+case class Corporation(corpId: Int, corpName: String, isFavorite: Boolean)
 
+case class Transaction(txId: Int, corpId: Int, txType: String, price: Long, createdAt: DateTime)
 
-case class Transaction(
-    txId: Int,
-    corpId : Int,
-    txType: String,
-    price: Long,
-    date: DateTime
-)
+//FIXME make txType and wishType as ENUM like thing
+case class Wish(wishId: Int, wishType: String, targetPrice: Long, createdAt: DateTime)
