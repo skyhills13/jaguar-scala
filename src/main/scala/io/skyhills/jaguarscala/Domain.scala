910b96f88af6677ef20ea309f8d7e673d8ca3c8a
박소은 <skyhills13@gmail.com>
2018-07-09 22:54:30 +0900

    #17 : add test api for getting data from db

--- src/main/scala/io/skyhills/jaguarscala/Domain.scala
+++ src/main/scala/io/skyhills/jaguarscala/Domain.scala
@@ -1,12 +1,19 @@
 package io.skyhills.jaguarscala
 
+import cats.effect.IO
 import com.github.nscala_time.time.Imports.DateTime
+import org.http4s.{EntityDecoder, EntityEncoder}
+import org.http4s.circe.{jsonEncoderOf, jsonOf}
 
 /**
   * Created by soeunpark on 2018. 6. 3...
   */
 case class Corporation(corpId: Int, corpName: String, isFavorite: Boolean)
 
+//object Corporation {
+//    implicit def corporationEncoder: EntityEncoder[IO, Corporation] = ???
+//}
+//
 case class Transaction(txId: Int, corpId: Int, txType: String, price: Long, createdAt: DateTime)
 
 //FIXME make txType and wishType as ENUM like thing
--- src/main/scala/io/skyhills/jaguarscala/service/HelloWorldService.scala
+++ src/main/scala/io/skyhills/jaguarscala/service/HelloWorldService.scala
@@ -1,12 +1,10 @@
 package io.skyhills.jaguarscala.service
 
 import cats.effect.Effect
-import fs2.Stream
 import io.circe.{Encoder, Json}
 import io.skyhills.jaguarscala.repository.TestRepository
-import org.http4s.{HttpService, MediaType}
+import org.http4s.HttpService
 import org.http4s.circe._
-import io.circe.syntax._
 import io.skyhills.jaguarscala.Corporation
 import org.http4s.dsl.Http4sDsl
 import io.circe.generic.auto._
@@ -21,6 +19,9 @@ class HelloWorldService[F[_] : Effect](repository: TestRepository) extends Http4
 
             case GET -> Root / "test" =>
                 Ok(Corporation(1, "aaa", true).asJson)
+
+//            case GET -> Root / "test" /  LongVar(corpId) =>
+//                Ok(repository.getCorp(corpId).map(_.asJson))
         }
     }
 }
